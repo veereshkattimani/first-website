17d7c90a2357b5a69e648838feedf29927a39149
Veeresh <veereshnk274@gmail.com>
2026-01-13 22:35:56 +0530

    added new code

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import os
 print(os.listdir())
 print(os.listdir('./'))
+
+
+
+try:
+    print(os.listdir('./git_tutorial'))
+except FileNotFoundError:
+    print('git_tutorial directory not found')
+
